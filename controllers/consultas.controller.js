af2a3b114f61bfae9b2856b9ab86fde379c56994
JordyV23 <jordyvargas0123@gmail.com>
2023-04-13 10:31:01 -0600

    Rutas para obtener todas las consultas creadas y para busqueda de consulta por id, creadas

--- controllers/consultas.controller.js
+++ controllers/consultas.controller.js
@@ -3,6 +3,40 @@ const Consultas = require("../models/consultas.model");
 const { error500, sinCoincidencias, error400 } = require("../helpers/resp");
 const pacientesModel = require("../models/pacientes.model");
 
+const getConsultas = async (req = request, res = response) => {
+  try {
+    const consultas = await Consultas.find({ finalizada: false });
+
+    return res.status(200).json({
+      success: true,
+      message: "Accion ejecutada exitosamente",
+      consultas: consultas,
+    });
+  } catch (error) {
+    console.log(error);
+    return error500(res);
+  }
+};
+
+const getConsulta = async (req = request, res = response) => {
+  try {
+    const { idConsulta } = req.params;
+    const consulta = await Consultas.findById(idConsulta);
+
+    if (!consulta) {
+      return error400(res, "Consulta no existente");
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Accion ejecutada exitosamente",
+      consulta: consulta,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const inicializarConsulta = async (req = request, res = response) => {
   try {
     const {
@@ -46,6 +80,35 @@ const inicializarConsulta = async (req = request, res = response) => {
   }
 };
 
+const diagnostico = async (req = request, res = response) => {
+  try {
+    const { idDiagnostico, diagnostico, medicamentos, examenes } = req.body;
+
+    await Consultas.findOneAndUpdate(
+      { _id: idDiagnostico },
+      {
+        $set: {
+          diagnostico: diagnostico,
+          medicamentos: medicamentos,
+          examenes: examenes,
+          finalizada: true,
+        },
+      }
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "Diagnostico realizado exitosamente",
+    });
+  } catch (error) {
+    console.log(error);
+    return error500(res);
+  }
+};
+
 module.exports = {
   inicializarConsulta,
+  diagnostico,
+  getConsultas,
+  getConsulta,
 };
--- routes/consultas.routes.js
+++ routes/consultas.routes.js
@@ -1,9 +1,21 @@
 const { Router } = require("express");
-const { inicializarConsulta } = require("../controllers/consultas.controller");
+const {
+  inicializarConsulta,
+  diagnostico,
+  getConsultas,
+  getConsulta,
+} = require("../controllers/consultas.controller");
 const router = Router();
 
+router.get("/getAll", getConsultas);
 
+//validar longitud de id que sea de 12 y que sea string
+router.get("/get/:idConsulta",getConsulta)
+
+//validar que el rol sea enfermera
 router.post("/inicializarConsulta", inicializarConsulta);
 
+//validar que el rol sea doctor
+router.post("/diagnostico", diagnostico);
 
 module.exports = router;
